41a8435a9ef67ece41a134adaed94a9a26f012b2
Levi-Brunerclear <lwbruner91@gmail.com>
2020-03-23 16:09:29 -0600

    Initial build, can successfully check out

---
+++ Models/Library.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+
+namespace console_library.Models
+{
+  public class Library
+  {
+    //Properties
+    public string Location { get; set; }
+    public string Name { get; set; }
+    private List<Book> Books { get; set; }
+    public List<Book> CheckedOut { get; set; }
+    //Methods
+    public void PrintBooks()
+    {
+      for (int i = 0; i < Books.Count; i++)
+      {
+        Console.WriteLine($"{i + 1} {Books[i].Title} - {Books[i].Author}");
+      }
+    }
+    public void AddBook(Book book)
+    {
+      Books.Add(book);
+    }
+    public void Checkout(string selection)
+    {
+      Book selectedBook = ValidateBook(selection, Books);
+      {
+        if (selectedBook == null)
+        {
+          Console.Clear();
+          System.Console.WriteLine(@"Invalid Selection");
+          return;
+        }
+        selectedBook.Available = false;
+        CheckedOut.Add(selectedBook);
+        Books.Remove(selectedBook);
+        Console.WriteLine("Successfully checked out.");
+      }
+    }
+    private Book ValidateBook(string selection, List<Book> booklist)
+    {
+      int bookIndex = 0;
+      bool valid = Int32.TryParse(selection, out bookIndex);
+      if (!valid || bookIndex < 1 || bookIndex > booklist.Count)
+      {
+        return null;
+      }
+      return booklist[bookIndex - 1];
+    }
+    //Constructor
+    public Library(string location, string name)
+    {
+      Location = location;
+      Name = name;
+      Books = new List<Book>();
+      CheckedOut = new List<Book>();
+    }
+
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using console_library.Models;
+
+namespace console_library
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Book endersGame = new Book("Ender's Game", "Orson S. Card");
+      Library Levi = new Library("303 W Pennwood", "Levi's Public Library");
+      Levi.AddBook(endersGame);
+      Levi.AddBook(new Book("Can't Hurt Me", "David Goggins"));
+      Levi.AddBook(new Book("Times of Contempt", "Andrzej Aapkowski"));
+      Console.WriteLine($"Welcome to {Levi.Name} located at {Levi.Location}.\n Available Books: ");
+      Levi.PrintBooks();
+      Console.WriteLine("Please select a book number to checkout, (Q)uit, or (R)eturn a book");
+      string selection = Console.ReadLine();
+      Levi.Checkout(selection);
+    }
+  }
+}
